3ec11e1fae0a28ddea6377eb98f4f79859eb1980
Brooks Johnson <brooks.johnson@pm.me>
2021-05-11 11:33:55 -0700

    Remove a test to keep things simple

--- src/components/my-component/my-component.e2e.ts
+++ src/components/my-component/my-component.e2e.ts
@@ -16,25 +16,4 @@ describe('Exercise 1', () => {
       </my-component>
     `);
   });
-
-  it('renders changes to the name data', async () => {
-    const page = await newE2EPage();
-
-    await page.setContent('<my-component></my-component>');
-    const component = await page.find('my-component');
-    const element = await page.find('my-component >>> div');
-    expect(element.textContent).toEqual(`Hello, World! I'm John`);
-
-    component.setProperty('first', 'James');
-    await page.waitForChanges();
-    expect(element.textContent).toEqual(`Hello, World! I'm James`);
-
-    component.setProperty('last', 'Quincy');
-    await page.waitForChanges();
-    expect(element.textContent).toEqual(`Hello, World! I'm James Quincy`);
-
-    component.setProperty('middle', 'Earl');
-    await page.waitForChanges();
-    expect(element.textContent).toEqual(`Hello, World! I'm James Earl Quincy`);
-  });
 });
