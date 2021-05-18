b9806437d4200bbc04c3adf6a125bfdc7976d15e
Brooks Johnson <brooks.johnson@pm.me>
2021-05-18 13:09:44 -0700

    Remove the readme build output

--- stencil.config.ts
+++ stencil.config.ts
@@ -11,9 +11,6 @@ export const config: Config = {
       type: 'dist-custom-elements-bundle',
     },
     {
-      type: 'docs-readme',
-    },
-    {
       type: 'www',
       serviceWorker: null, // disable service workers
     },
